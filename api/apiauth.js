88520b76ffc833aa6b588000cf3e1b4f6f6093fa
Robson Fagundes <robsonfagundes@gmail.com>
2017-02-07 15:22:46 -0200

    Start Api on Node

---
+++ api/apiauth.js
@@ -0,0 +1,17 @@
+/**
+ * apiauth.js - Start API on Node
+ *
+ * @autor: Robson Fagundes - robsonfagundes@gmail.com
+ *
+ */
+
+'use strict'
+
+var http = require('http');
+var app = require('./config/express')();
+
+require('./config/database.js')('mongodb://localhost:27017/apiauth');
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Api Auth: Node and Express server listen on port ' + app.get("port"));
+});
